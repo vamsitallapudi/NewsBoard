6e1193b46ab9b5070e2d8faa5135d806990952c6
Vamsi Tallapudi <vamsi@coderefer.com>
2018-01-16 15:56:11 +0530

    read file using kotlin from assets folder

--- app/src/main/java/com/coderefer/newsboard/MainActivity.kt
+++ app/src/main/java/com/coderefer/newsboard/MainActivity.kt
@@ -3,15 +3,30 @@ package com.coderefer.newsboard
 import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.coderefer.newsboard.databinding.ActivityMainBinding
-
+import java.io.InputStream
 class MainActivity : AppCompatActivity() {
 
     var mBinding: ActivityMainBinding? = null
+    private val TAG : String = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
+        readJsonFromKotlinFile()
+
     }
+
+    private fun readJsonFromKotlinFile() {
+        try {
+            val inputStream:InputStream = assets.open("news_data_file.json")
+            val inputString = inputStream.bufferedReader().use{it.readText()}
+            Log.d(TAG,inputString)
+        } catch (e:Exception){
+            Log.d(TAG, e.toString())
+        }
+    }
+
 }
