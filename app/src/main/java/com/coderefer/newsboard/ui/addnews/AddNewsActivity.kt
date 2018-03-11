32ed62ea549649c8e4d773687fe138aae23aae84
Vamsi Tallapudi <vamsi@coderefer.com>
2018-03-11 15:20:05 +0530

    Room library integrated and AddNews Activity and Fragments created

---
+++ app/src/main/java/com/coderefer/newsboard/ui/addnews/AddNewsActivity.kt
@@ -0,0 +1,14 @@
+package com.coderefer.newsboard.ui.addnews
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.coderefer.newsboard.R
+
+class AddNewsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_news)
+
+    }
+}
---
+++ app/src/main/java/com/coderefer/newsboard/ui/addnews/AddNewsFragment.kt
@@ -0,0 +1,5 @@
+package com.coderefer.newsboard.ui.addnews
+
+/**
+ * Created by vamsitallapudi on 11/03/18.
+ */
